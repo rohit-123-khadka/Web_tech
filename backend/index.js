5c255d86d00b0cb90c52a0117da01e70d0e64a3f
rohit-123-khadka <khadkarohit2006@gmail.com>
2026-02-14 23:52:28 +0545

    backend set up

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+});
+
+app.get("/", (req, res) => {
+    res.send("Rohit Kumar Khadka.. Backend is Running")
+})
+
+
