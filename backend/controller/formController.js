41d757af31ec1ce184028ab05ae25c2262b857ef
rohit-123-khadka <khadkarohit2006@gmail.com>
2026-02-15 00:02:50 +0545

    model and controler and route code

--- backend/controller/formController.js
+++ backend/controller/formController.js
@@ -8,17 +8,30 @@ const createForm = async (req, res) => {
         succss: false,
         message: "All Fields are required",
       });
-    };
-
-    const existingEmail = await Form.findOne({email: email});
-    if(existingEmail){
-        return res.status(400).json({
-            succss: false,
-            message: "This email already exist"
-        });
     }
 
+    const existingEmail = await Form.findOne({ email: email });
+    if (existingEmail) {
+      return res.status(400).json({
+        succss: false,
+        message: "This email already exist",
+      });
+    }
 
+    const newForm = new Form({
+      fullName,
+      email,
+      phoneNumber,
+      selectedProgram,
+    });
+
+    await newForm.save();
+
+    return res.status(200).json({
+      succss: true,
+      message: "New Form Submitted successfully",
+      newForm,
+    });
   } catch (error) {
     return res.status(400).json({
       succss: false,
--- backend/router/formRoutes.js
+++ backend/router/formRoutes.js
@@ -1,3 +1,7 @@
-const express = require("express").Router();
+const router = require("express").Router();
+const formController = require("../controller/formController");
 
+router.post("/create", formController.createForm);
+
+module.export = router;
 
